dff9950340b890a1bd1c910655eb07c3e552e887
Thiago F. Melo <m4tri_x@hotmail.com>
2024-02-21 16:34:31 -0300

    Create idor.php

---
+++ .gfphp/idor.php
@@ -0,0 +1,21 @@
+<?php
+
+$pattern = array(
+    'idor' => array(
+        '/[?&]d=/i',
+        '/[?&]user=/i',
+        '/[?&]account=/i',
+        '/[?&]number=/i',
+        '/[?&]order=/i',
+        '/[?&]no=/i',
+        '/[?&]doc=/i',
+        '/[?&]key=/i',
+        '/[?&]email=/i',
+        '/[?&]group=/i',
+        '/[?&]profile=/i',
+        '/[?&]edit=/i',
+        '/[?&]report=/i'
+    )
+);
+
+?>
