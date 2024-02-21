649df7f84aefb2bd2f6f30f3b1bd508240111bc6
Thiago F. Melo <m4tri_x@hotmail.com>
2024-02-21 16:35:13 -0300

    Create rce.php

---
+++ .gfphp/rce.php
@@ -0,0 +1,40 @@
+<?php
+
+$pattern = array(
+    'rce' => array(
+        '/[?&]daemon=/i',
+        '/[?&]upload=/i',
+        '/[?&]dir=/i',
+        '/[?&]download=/i',
+        '/[?&]log=/i',
+        '/[?&]ip=/i',
+        '/[?&]cli=/i',
+        '/[?&]cmd=/i',
+        '/[?&]exec=/i',
+        '/[?&]command=/i',
+        '/[?&]execute=/i',
+        '/[?&]ping=/i',
+        '/[?&]query=/i',
+        '/[?&]jump=/i',
+        '/[?&]code=/i',
+        '/[?&]reg=/i',
+        '/[?&]do=/i',
+        '/[?&]func=/i',
+        '/[?&]arg=/i',
+        '/[?&]option=/i',
+        '/[?&]load=/i',
+        '/[?&]process=/i',
+        '/[?&]step=/i',
+        '/[?&]read=/i',
+        '/[?&]function=/i',
+        '/[?&]req=/i',
+        '/[?&]feature=/i',
+        '/[?&]exe=/i',
+        '/[?&]module=/i',
+        '/[?&]payload=/i',
+        '/[?&]run=/i',
+        '/[?&]print=/i'
+    )
+);
+
+?>
